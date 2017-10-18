a96972081ebbce9710dc91249273762c2aa03fea
Philipp Jenke <philipp.jenke@gmail.com>
2017-10-18 09:51:46 +0200

    Restuctured modules, adjusted for AndroidStudio 3

---
+++ android/lib_vuforia/src/main/java/edu/hawhamburg/vuforia/DefaultVuforiaScene.java
@@ -0,0 +1,18 @@
+package edu.hawhamburg.vuforia;
+
+import edu.hawhamburg.shared.misc.DefaultScene;
+import edu.hawhamburg.shared.scenegraph.InnerNode;
+
+/**
+ * Dummy implementation of a scene with a Vuforia marker
+ *
+ * @author Philipp Jenke
+ */
+public class DefaultVuforiaScene extends DefaultScene {
+    @Override
+    public void onSetup(InnerNode rootNode) {
+        VuforiaMarkerNode marker = new VuforiaMarkerNode("elphi");
+        getRoot().addChild(marker);
+        super.onSetup(marker);
+    }
+}
